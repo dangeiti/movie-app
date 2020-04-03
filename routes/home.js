0c9bf209b312155857f9b70c9813aeb7d9c7170a
unknown <geitipogi@yahoo.com>
2020-04-03 12:02:59 -0700

    added some icons

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,7 @@ const Movie = require('../models/Movie.model.js');
 const home = async (req, res) => {
   const documents = await Movie.find().exec();
   // console.log(documents)
+
   res.render('home', { movies: documents });
 }
 
